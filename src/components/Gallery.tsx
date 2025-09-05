cd0812080afaf53d2821ebbade7eae91c28bec5b
theDriftcode <98360334+theDriftcode@users.noreply.github.com>
2025-09-05 11:06:26 -0700

    Mobile gallery test

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -44,7 +44,7 @@ export default function Gallery() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3">
           {galleryImages.map((image) => (
             <div 
               key={image.id} 
